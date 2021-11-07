8b0c7ff0d46c5789308f068b309ec0efc754ef55
wj-111 <1119070771@qq.com>
2021-11-07 17:05:17 +0800

    引入elementui、编写footer公共组件、建立公共common.css样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import less from 'less'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import './assets/style/common.css'
+
 Vue.use(less)
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
