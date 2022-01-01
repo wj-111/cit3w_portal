abdbb4d2d2961a70089eeffc88f6c321383c033d
wj-111 <1119070771@qq.com>
2022-01-01 22:37:18 +0800

    更新了新闻模块列表内容，临时修改了一下服务域名地址

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: '/api/v1', // 开发模式下
-    baseURL: 'http://106.15.42.201:8900/', // 线上模式
+    // baseURL: 'http://106.15.42.201:8900/', // 线上模式(yc)
+    baseURL: 'https://api.aerowang.cn/xanadu/', // 线上模式(guanfang)
     // baseURL: 'http://localhost:3000/api/v1',
     // 请求超时时间
     timeout: 10000
--- src/views/news/news.vue
+++ src/views/news/news.vue
@@ -9,29 +9,30 @@
             <div class="news-list">
                 <el-tabs class="list-left" v-model="pageInfo.activeName" @tab-click="handleClick">
                     <el-tab-pane :label="newsTabs[0].name" :name="newsTabs[0].id">
-                        <news-list :items="newsItems.list" v-if="pageInfo.activeName===newsTabs[0].id"></news-list>
+                        <NewsList :items="newsItems.list"></NewsList>
                     </el-tab-pane>
                     <el-tab-pane :label="newsTabs[1].name" :name="newsTabs[1].id">
-                        <news-list :items="newsItems.list" v-if="pageInfo.activeName===newsTabs[1].id"></news-list>
+                        <NewsList :items="newsItems.list"></NewsList>
                     </el-tab-pane>
                     <el-tab-pane :label="newsTabs[2].name" :name="newsTabs[2].id">
-                        <news-list :items="newsItems.list" v-if="pageInfo.activeName===newsTabs[2].id"></news-list>
+                        <NewsList :items="newsItems.list"></NewsList>
                     </el-tab-pane>
-                    <!-- <el-pagination class="pagination" background @current-change="handleCurrentChange" :current-page.sync="pageInfo.pagenum"
+                    <!-- 多页内容是否能回退至顶部待验证 -->
+                    <el-pagination class="pagination" background @current-change="handleCurrentChange" :current-page.sync="pageInfo.pagenum"
                                    :page-size="pageInfo.pagesize" layout="prev, pager, next, jumper" :total="newsItems.total"
                                    :hide-on-single-page="singlePage"
                                    v-scroll-to="{ element: '.news-container',duration: 300, easing: 'ease',offset: -40  }">
-                    </el-pagination> -->
+                    </el-pagination>
                 </el-tabs>
-                <!-- <div class="list-right">
+                <div class="list-right">
                     <div class="search-by-date">
                         <p>按日期搜索：</p>
                         <el-date-picker v-model="pageInfo.selectDate" type="month" placeholder="选择日期" value-format="yyyy-MM"
                                         @change="searchByDate(pageInfo.selectDate)">
                         </el-date-picker>
                     </div>
-                    <hot-news></hot-news>
-                </div> -->
+                    <!-- <hot-news></hot-news> -->
+                </div>
             </div>
         </div>
 
@@ -43,13 +44,17 @@ import AwHeader from '../../publicComponents/Header'
 import AwFooter from '../../publicComponents/Footer'
 import TitleSearch from './TitleSearch'
 import NewsCard from './NewsCard'
+import NewsList from './NewsList'
+
+
 export default {
     name: "news",
     components: {
         AwHeader,
         AwFooter,
         TitleSearch,
-        NewsCard
+        NewsCard,
+        NewsList
     },
     data() {
         return {
@@ -83,22 +88,46 @@ export default {
             this.getNewsItems()
         },
 
-        // 获取新闻
-        async getNewsItems() {
-            this.newsItems = {}
-            // const { data: res } = await this.$http.get('/article/fuzzySearch?pageNum=1&pageSize=14&search=' + queryString)
-            // if (res.status !== 200) {
-            //     this.newsItems = {}
-            // } else {
-            //     // this.$message.success('获取成功')
-            //     this.newsItems = res.data
-            //     if (this.newsItems.total <= this.newsItems.limit) {
-            //         this.singlePage = true
-            //     }
-            // }
+        // 新闻列表页码切换
+        handleCurrentChange(val) {
+            // console.log(`当前页: ${val}`)
+            this.getNewsItems()
         },
 
-    }
+
+        async getNewsItems() {
+            const { data: res } = await this.$http.get('/web/newslist', { params: this.pageInfo })
+            if (res.status !== 200) {
+                this.newsItems = {}
+            } else {
+                // this.$message.success('获取成功')
+                this.newsItems = res.data
+                if (this.newsItems.total <= this.newsItems.limit) {
+                    this.singlePage = true
+                }
+            }
+        },
+
+        // 获取新闻
+        // async getNewsItems() {
+        //     this.newsItems = {}
+        // https://api.aerowang.cn/xanadu/web/newslist?activeName=1&pagenum=1&pagesize=10&selectDate=
+        // const { data: res } = await this.$http.get('/article/fuzzySearch?pageNum=1&pageSize=14&search=' + queryString)
+        // if (res.status !== 200) {
+        //     this.newsItems = {}
+        // } else {
+        //     // this.$message.success('获取成功')
+        //     this.newsItems = res.data
+        //     if (this.newsItems.total <= this.newsItems.limit) {
+        //         this.singlePage = true
+        //     }
+        // }
+        // },
+    },
+    created() {
+        this.getNewsItems()
+        // this.getRecomNews()
+    },
 };
 </script>
 
@@ -113,96 +142,39 @@ export default {
     //background: #d3dce6;
     min-height: 580px;
     margin: 0 auto;
+}
 
-    //   .news-list {
-    //     position: relative;
-    //     display: flex;
-    //     justify-content: space-between;
-    //   }
+.news-list {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+}
 
-    //   .news-list:after {
-    //     content: "";
-    //     position: absolute;
-    //     left: 0;
-    //     top: 43px;
-    //     width: 100%;
-    //     height: 2px;
-    //     background-color: #E4E7ED;
-    //     z-index: 1;
-    //   }
+.news-list:after {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 43px;
+    width: 100%;
+    height: 2px;
+    background-color: #e4e7ed;
+    z-index: 1;
+}
 
-    //   /deep/ .el-tabs__header {
-    //     height: 60px;
-    //   }
+// 调整内容顶部留白高度
+/deep/ .el-tabs__header {
+    height: 60px;
+}
 
-    //   /deep/ .el-tabs__active-bar {
-    //     bottom: 5px;
-    //     //height: 3px;
-    //   }
+// 调整激活栏选项框高度
+/deep/ .el-tabs__active-bar {
+    bottom: 5px;
+    //height: 3px;
+}
 
-    //   /deep/ .el-tabs__item {
-    //     height: 50px;
-    //     line-height: 50px;
-    //     font-weight: 600;
-    //   }
-
-    //   .list-left {
-    //     width: 860px;
-
-    //     /deep/ .el-tabs__content {
-    //       height: auto;
-    //       //background-color: #d2d3d4;
-    //     }
-    //   }
-
-    //   .el-pagination {
-    //     display: flex;
-    //     justify-content: center;
-    //     margin-top: 30px;
-    //     margin-bottom: 50px;
-    //   }
-
-    //   .list-right {
-    //     margin-left: 50px;
-
-    //     .search-by-date {
-    //       //padding: 6px 0;
-    //       display: flex;
-    //       height: 50px;
-    //       align-items: center;
-    //       justify-content: flex-end;
-    //       font-size: 14px;
-    //       margin-bottom: 25px;
-
-    //       p {
-    //         white-space: nowrap;
-    //         position: relative;
-    //         //right: -30px;
-    //       }
-
-    //       .el-date-editor.el-input {
-    //         width: 160px;
-    //       }
-
-    //       /deep/ .el-input__inner {
-    //         width: 160px;
-    //         height: 30px;
-    //         line-height: 30px;
-    //       }
-
-    //       /deep/ .el-input__prefix, /deep/ .el-input__suffix {
-    //         top: -4px;
-    //       }
-
-    //       ///deep/ .el-input__suffix{
-    //       //  right: 50px;
-    //       //}
-    //     }
-
-    //     /deep/ .el-card__body {
-    //       padding-top: 0;
-    //     }
-
-    //   }
+/deep/ .el-tabs__item {
+    height: 50px;
+    line-height: 50px;
+    font-weight: 600;
 }
 </style>
